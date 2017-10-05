ce950def82fa2c5bc3606ccaf88bfa1bfac03823
571730 <571730@stud.hvl.no>
2017-10-05 11:19:45 +0200

    fikset xml innlesning med for loop

--- script.js
+++ script.js
@@ -12,17 +12,30 @@ function loadDoc() {
         var i;
         var xmlDoc = xml.responseXML;
         var content = "<h1 class='tittel'>Vår Problemstilling</h1>";
-        //var x = xmlDoc.getElementsByTagName("txt");
+        var testContent = "<h1 class='tittel'>Vår problemstilling</h1>";
+        var x = xmlDoc.getElementsByTagName("tag");
         //document.write(x.getElementsByTagName("problemstilling").textContent);
+        /*
         var problemstilling = "<p>" + xmlDoc.getElementsByTagName("problemstilling")[0].childNodes[0].nodeValue + "</p>";
         var problemstilling2 = "<p>" + xmlDoc.getElementsByTagName("problemstilling2")[0].childNodes[0].nodeValue + "</p>";
         var omProsjektet = "<p>" + xmlDoc.getElementsByTagName("omProsjektet")[0].childNodes[0].nodeValue + "</p>";
         var hensikt = "<p>" + xmlDoc.getElementsByTagName("prosjektHensikt")[0].childNodes[0].nodeValue + "</p>";
         var fremtid = "<p>" + xmlDoc.getElementsByTagName("fremtid")[0].childNodes[0].nodeValue + "</p>";
         var fremtid2 = "<p>" + xmlDoc.getElementsByTagName("fremtid2")[0].childNodes[0].nodeValue + "</p>";
+        */
 
-        content = content + problemstilling + problemstilling2 + "<h4>Om prosjektet</h4>" + omProsjektet +
-            "<h4>Hensikt</h4>" + hensikt + "<h4>Fremtidige utvidelser</h4>" + fremtid + fremtid2;
+        for (i=0; i<x.length; i++){
+            if (x[i].getElementsByTagName("tittel")[0].childNodes[0].nodeValue.length>2){
+                testContent = testContent + "<h4>" + x[i].getElementsByTagName("tittel")[0].childNodes[0].nodeValue + "</h4>"
+                    + "<p>" + x[i].getElementsByTagName("tekst")[0].childNodes[0].nodeValue + "</p>";
+            }else {
+                testContent = testContent + "<p>" + x[i].getElementsByTagName("tekst")[0].childNodes[0].nodeValue + "</p>";
+            }
 
-        document.getElementById("demo").innerHTML = content;
+        }
+
+        //content = content + problemstilling + problemstilling2 + "<h4>Om prosjektet</h4>" + omProsjektet +
+         //   "<h4>Hensikt</h4>" + hensikt + "<h4>Fremtidige utvidelser</h4>" + fremtid + fremtid2;
+
+        document.getElementById("demo").innerHTML = testContent;
     }
