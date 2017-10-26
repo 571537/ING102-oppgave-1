870547964c4e970f84eeedb75ef5ed0b191dca24
571730 <571730@stud.hvl.no>
2017-10-26 15:17:39 +0200

    Nye funksjoner på Info

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 
-
+//Funksjon som laster inn tekst fra XML fil, denne vil bare fungere når sidene er lastet opp på en server
+//Se linken til HVL serveren for å se resultatet
 function loadDoc() {
+        document.getElementById("demo").style.display = "block";
+        document.getElementById("loadDoc").style.display = "none";
+        document.getElementById("hideDoc").style.display = "block";
         var xhttp = new XMLHttpRequest();
         xhttp.onreadystatechange = function() {
             if (this.readyState == 4 && this.status == 200) {
@@ -29,4 +33,19 @@ function loadDoc() {
         document.getElementById("demo").innerHTML = testContent;
     }
 
+function seBildet() {
+        document.getElementById("ganttContainer").style.display = "block";
+        document.getElementById("seBilde").style.display = "none";
+        document.getElementById("lukkBilde").style.display = "block";
+}
+function lukkBildet() {
+    document.getElementById("ganttContainer").style.display = "none";
+    document.getElementById("seBilde").style.display = "block";
+    document.getElementById("lukkBilde").style.display = "none";
+}
+function hideDoc() {
+    document.getElementById("demo").style.display = "none";
+    document.getElementById("loadDoc").style.display = "block";
+    document.getElementById("hideDoc").style.display = "none";
 
+}
